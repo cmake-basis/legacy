1ab3a138b6db3c250eae6bfc647a827e30270ba5
Andreas Schuh <andreas.schuh.84@googlemail.com>
2011-07-21 20:26:52 +0000

    Changed copyright notice in file headers such that Web page link works in generated HTML documentation. Changed Doxygen tags from \tag to @tag.
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/trunk@697 3c61e6fb-ae43-4039-8cf7-c895b3257837

--- src/cmake/FindMatlabNiftiTools.cmake
+++ src/cmake/FindMatlabNiftiTools.cmake
@@ -22,7 +22,7 @@
 #! - MatlabNiftiTools_INCLUDES     Alias for MatlabNiftiTools_INCLUDE_DIRS (not cached).
 #!
 #! Copyright (c) 2011 University of Pennsylvania. All rights reserved.
-#! See COPYING file or https://www.rad.upenn.edu/sbia/software/license.html.
+#! See https://www.rad.upenn.edu/sbia/software/license.html or COPYING file.
 #!
 #! Contact: SBIA Group <sbia-software at uphs.upenn.edu>
 ##############################################################################
