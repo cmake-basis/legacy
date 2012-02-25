75f73189e13c51cd78ef81607bf1dddf0a558a15
Andreas Schuh <andreas.schuh.84@googlemail.com>
2012-02-25 20:04:57 +0000

    Bump version to 1.1.0.
    git-svn-id: https://sbia-svn.uphs.upenn.edu/projects/BASIS/branches/basis-1.1@1978 3c61e6fb-ae43-4039-8cf7-c895b3257837

--- BasisProject.cmake
+++ BasisProject.cmake
@@ -48,7 +48,7 @@
 
 basis_project (
   NAME        "BASIS"
-  VERSION     "0.0.0"
+  VERSION     "1.1.0"
   DESCRIPTION "This package implements and supports the development of "
               "software which follows the SBIA Build system And Software "
               "Implementation Standard (BASIS)."
